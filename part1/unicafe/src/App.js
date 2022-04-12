ad9cbd005158771bcaab74f11950b820db28f69e
malinhaggblom <malin.haggblom@centria.fi>
2022-04-12 11:11:08 +0300

    part 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -34,6 +34,9 @@ const Statistics = (props) => {
       <StatisticsValue text="good" value={props.good}/>
       <StatisticsValue text="neutral" value={props.neutral}/>
       <StatisticsValue text="bad" value={props.bad}/>
+      <StatisticsValue text="all" value={props.total}/>
+      <StatisticsValue text="average" value={(props.good - props.bad)/(props.total)}/>
+      <StatisticsValue text="positive" value={(props.good/props.total * 100) + '%'}/>
     </div>
   )
 }
@@ -42,6 +45,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
 
   return (
     <div>
@@ -49,7 +53,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good"/>
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="bad"/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={(good - bad)/(total)} positive={(good/total*100)}/>
     </div>
   )
 }
