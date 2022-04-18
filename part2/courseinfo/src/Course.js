a4fea9e440fe32e16a39d9bbe9514b6ef032ea12
Malin Häggblom <malin.haggblom@centria.fi>
2022-04-18 14:40:27 +0300

    part 2.3

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -4,7 +4,8 @@ const Course = ({course}) => {
     console.log(course)
     const content = course.parts.map(part => 
         (<Part key={part.id} name={part.name} exercises={part.exercises}/>))
-    const total = course.parts.reduce((s, p) => s= s+p.exercises,0)
+    const total = 
+        course.parts.reduce((s, p) => s= s+p.exercises,0)
     return (
         <div>
             <h1>{course.name}</h1>
