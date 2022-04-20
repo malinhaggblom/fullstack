9c0ee2e081e526729c85c0eb4d4a2be05d90aaa3
Malin Häggblom <malin.haggblom@centria.fi>
2022-04-20 21:30:54 +0300

    part 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,38 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
-const App = () => {
+const App = (props) => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  
+  const addPerson = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    console.log(event.target.value)
+    event.preventDefault();
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
+        <form onSubmit={handleSubmit}>
+          <div>
+            name: <input onChange={addPerson} value={newName}/>
+          </div>
+          <div>
+            <button type="submit">add</button>
         </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+        </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => {
+        return <p key={person.name}>{person.name}</p>;
+      })}
     </div>
   )
 }
