22f5369c6ead12ec2982b6c52a48c957782fd980
Malin Häggblom <malin.haggblom@centria.fi>
2022-04-26 13:10:29 +0300

    part 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -20,19 +20,6 @@ const App = (props) => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            name: <input onChange={addPerson} value={newName}/>
-          </div>
-          <div>
-            <button type="submit">add</button>
-        </div>
-        </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => {
-        return <p key={person.name}>{person.name}</p>;
-      })}
     </div>
   )
 }
