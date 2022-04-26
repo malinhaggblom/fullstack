7a29f730b9a344c7c36e9ab2fe8d9dab2cab9fb3
Malin Häggblom <malin.haggblom@centria.fi>
2022-04-26 13:21:22 +0300

    part 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,11 +6,14 @@ const App = (props) => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   
   const addPerson = (event) => {
     setNewName(event.target.value)
   }
-
+  const addNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
   const handleSubmit = (event) => {
     console.log(event.target.value)
     event.preventDefault();
@@ -20,27 +23,28 @@ const App = (props) => {
     {
       alert(newName + ' is already added to phonebook')
       setNewName("")
+      setNewNumber("")
       return
     }
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber}
     setPersons(persons.concat(newPerson))
     setNewName("")
+    setNewNumber("")
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
         <form onSubmit={handleSubmit}>
-          <div>
-            name: <input onChange={addPerson} value={newName}/>
-          </div>
+          <div>name: <input onChange={addPerson} value={newName}/></div>
+          <div>number: <input onChange={addNumber} value={newNumber}/></div>
           <div>
             <button type="submit">add</button>
         </div>
         </form>
       <h2>Numbers</h2>
       {persons.map((person) => {
-        return <p key={person.name}>{person.name}</p>;
+        return <p key={person.name}>{person.name} {person.number}</p>;
       })}
     </div>
   )
